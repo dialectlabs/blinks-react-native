10ff0ad023359ba20af87809c2b22ba08074c6c0
Victoria Prusakova <prsvic@gmail.com>
2024-07-26 16:50:59 +0300

    add keyboard avoiding

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,43 @@
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
 import { BlinkInTheWalletIntegrationExample } from './Example';
 
 const actionUrl = 'https://dial.to/donate';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={styles.container}
+    <SafeAreaView style={styles.safeArea}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
-        <View style={styles.box}>
-          <BlinkInTheWalletIntegrationExample url={actionUrl} />
-        </View>
-      </ScrollView>
+        <ScrollView
+          style={styles.scrollView}
+          contentContainerStyle={styles.container}
+        >
+          <View style={styles.box}>
+            <BlinkInTheWalletIntegrationExample url={actionUrl} />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea:
+    Platform.OS === 'android'
+      ? {
+          paddingTop: StatusBar.currentHeight,
+          paddingBottom: 12,
+        }
+      : {},
   scrollView: {
     height: '100%',
     paddingHorizontal: 12,
